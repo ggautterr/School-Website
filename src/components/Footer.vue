61df53bd872ffbac62cfa15381a03f0245793d1b
unknown <aliqulovshaxboz2002@gmail.com>
2022-03-16 13:41:53 +0300

    Running Projects - ShaXBoZ

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer>
+        <div class="container">
+            <div class="1">
+                <div class="logo">
+                    <h3>Denov 4-son</h3>
+                    <p>kasb hunar maktabi</p>
+                </div>
+                <div class="text">
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                </div>
+                <div class="socially">
+                    <fa :icon=" ['fab', 'facebook'] "/>
+                    <fa :icon=" ['fab', 'telegram'] "/>
+                    <fa :icon=" ['fab', 'instagram'] "/>
+                </div>
+            </div>
+        </div>
+    </footer>   
+</template>
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+<style scoped>
+.socially {
+    font-size: 25px;
+}
+</style>
